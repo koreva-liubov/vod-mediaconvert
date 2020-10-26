5e7c659e502637c33a682a8e774789595e863e61
Liubov Koreva <LKoreva@vizrt.com>
2020-10-26 11:55:15 +0100

    Add new sample files to upload.py

--- upload.py
+++ upload.py
@@ -2,18 +2,18 @@ import boto3
 import logging
 from botocore.exceptions import ClientError
 
-def upload(filename):
+def upload(filepath, filename):
 	print("Uploading", filename, "...")
 	try:
 		s3 = boto3.resource('s3')
 		bucket = "koreva-liubov-input-bucket"
-		s3.meta.client.upload_file(filename, bucket, filename)
+		s3.meta.client.upload_file(filepath, bucket, filename)
 	except ClientError as e:
 		logging.error(e)
 	print("Uploaded successfully.")
 
-upload("10-sec-windsurfing.mp4")
-upload("10-sec-beach.mp4")
-upload("10-sec-lid.mp4")
+upload("mediafiles/Windsurfing.mov", "Windsurfing.mov")
+upload("mediafiles/Llama_drama_1080p.mp4", "Llama_drama_1080p.mp4")
+upload("mediafiles/Sample-DV.mxf", "Sample-DV.mxf")
 
 print("Done.")
